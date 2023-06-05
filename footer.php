f080f86bcda125be482a6c84c0bf7629a9653e25
ThomasViennet <viennet.t@gmail.com>
2023-06-05 14:12:12 +0200

    add waves

--- footer.php
+++ footer.php
@@ -14,8 +14,26 @@
 
 ?>
 <div id="particle-canvas">
+	<?php
+	if (is_page_template(array('templates/template-waves.php', 'templates/template-cover.php', 'templates/template-netlinking.php'))) {
+		echo
+		'<svg class="waves-2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto" transform="scale(1, -1) translate(0, -100)">
+				<defs>
+					<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" ></path>
+				</defs>
+				<g class="parallax">
+					<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7)"></use>
+					<use xlink:href="#gentle-wave" x="48" y="1" fill="white"></use>
+					<use xlink:href="#gentle-wave" x="48" y="2" fill="rgba(255,255,255,0.3)"></use>
+					<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)"></use>
+					<use xlink:href="#gentle-wave" x="48" y="4" fill="white"></use>
+					<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)"></use>
+					<use xlink:href="#gentle-wave" x="48" y="7" fill="white"></use>
+				</g>
+			</svg>';
+	}
+	?>
 	<footer id="site-footer" class="header-footer-group">
-
 		<div class="section-inner">
 			<div class="footer-credits">
 
@@ -57,22 +75,17 @@
 		</div><!-- .section-inner -->
 	</footer><!-- #site-footer -->
 </div>
-
-<?php if (is_page_template(array('templates/template-netlinking.php'))) {
-	echo '
-		
-		<script type="text/javascript" src="http://localhost:8888/referencime.fr/wp-content/themes/referencime/assets/js/netlinking.js"></script>
-		<script type="text/javascript">
-			var canvasDiv = document.getElementById(\'particle-canvas\');
-			var options = {
-				particleColor: \'#fff\',
-				interactive: true,
-				speed: \'slow\',
-				density: \'high\'
-			};
-			var particleCanvas = new ParticleNetwork(canvasDiv, options);
-		</script>';
-} ?>
+<script type="text/javascript" src="http://localhost:8888/referencime.fr/wp-content/themes/referencime/assets/js/netlinking.js"></script>
+<script type="text/javascript">
+	var canvasDiv = document.getElementById('particle-canvas');
+	var options = {
+		particleColor: '#fff',
+		interactive: true,
+		speed: 'slow',
+		density: 'high'
+	};
+	var particleCanvas = new ParticleNetwork(canvasDiv, options);
+</script>
 <?php wp_footer(); ?>
 
 </body>
--- template-parts/content-cover.php
+++ template-parts/content-cover.php
@@ -165,22 +165,6 @@
 	</div><!-- .cover-header -->
 
 	<div class="post-inner" id="post-inner">
-		<?php
-		if (is_page_template(array('templates/template-waves.php', 'templates/template-cover.php', 'templates/template-netlinking.php'))) {
-			echo
-			'<svg class="waves-2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto" transform="scale(1, -1) translate(0, -100)">
-				<defs>
-					<path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" ></path>
-				</defs>
-				<g class="parallax">
-					<use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7"></use>
-					<use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)"></use>
-					<use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)"></use>
-					<use xlink:href="#gentle-wave" x="48" y="7" fill="white"></use>
-				</g>
-			</svg>';
-		}
-		?>
 		<div class="entry-content">
 
 			<?php
