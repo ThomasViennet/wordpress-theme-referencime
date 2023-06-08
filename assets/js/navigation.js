da9e11da299426621b68c5ee08ab438a45127634
ThomasViennet <viennet.t@gmail.com>
2023-06-08 11:31:01 +0200

    blur h1

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -6,6 +6,6 @@ jQuery(function () {
 		windowTop > 10 ? jQuery('div.header-inner').addClass('afterScroll') : jQuery('div.header-inner').removeClass('afterScroll');
 		windowTop > 10 ? jQuery('div.site-logo').addClass('afterScroll') : jQuery('div.site-logo').removeClass('afterScroll');
 		// windowTop > 10 ? jQuery('.waves-nav').addClass('afterScroll') : jQuery('.waves-nav').removeClass('afterScroll');
-		windowTop > 10 ? jQuery('h1').addClass('afterScroll') : jQuery('h1').removeClass('afterScroll');
+		windowTop > 10 ? jQuery('header.entry-header').addClass('afterScroll') : jQuery('header.entry-header').removeClass('afterScroll');
 	});
 });
